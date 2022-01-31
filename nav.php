a0829cfc04fa0a07e9d242701379283301f4c1f1
adaliawilliams <Adalia.Williams@uvm.edu>
2022-01-31 16:31:11 -0500

    corrected error in nav.php

--- nav.php
+++ nav.php
@@ -1,23 +1,23 @@
 <nav>
-    <a class="<? php
+    <a class="<?php
     if (PATH_PARTS['filename'] == "index") {
         print 'activePage';
     }
     ?>" href="index.php">Home</a>
 
-    <a class="<? php
+    <a class="<?php
     if (PATH_PARTS['filename'] == "about") {
         print 'activePage';
         }
     ?>" href="about.php">About</a>
 
-    <a class="<? php
+    <a class="<?php
     if (PATH_PARTS['filename'] == "contact") {
         print 'activePage';
     }
     ?>" href="contact.php">Contact</a>
 
-    <a class="<? php
+    <a class="<?php
     if (PATH_PARTS['filename'] == "admin") {
         print 'activePage';
     }
